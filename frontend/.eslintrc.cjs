9c14e2313194cd8a47762a0e499c0c249a0f25ec
Aladin Zorgui <zorguiala1@gmail.com>
2025-05-05 04:19:11 +0100

    fix: update frontend ESLint configuration

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -4,16 +4,36 @@ module.exports = {
     browser: true,
     es2020: true,
     node: true,
+    jest: true
   },
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended'
   ],
   ignorePatterns: ['dist', 'node_modules'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
+  plugins: [
+    'react',
+    'react-hooks',
+    '@typescript-eslint'
+  ],
   rules: {
-    // Add any specific rules here
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn'
   },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
 }; 
